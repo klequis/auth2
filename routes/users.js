98ea3e95de88b7073c8eb669cf5b494c27114656
klequis <klequis@gmail.com>
2017-12-21 17:11:32 -0800

    moved routes to users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const passport = require('../passport')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+router.get('/signin', function(req, res) {
+  console.log('** app.get /signin **')
+  res.render('login', {'message': req.flash('message')});
+});
+router.post("/signin", passport.authenticate('local', {
+  successRedirect: '/success',
+  failureRedirect: '/fail',
+  failureFlash: true
+}), function(req, res, info) {
+  console.log('** app.post /signin: info **', info)
+  res.render('login/index', {'message': req.flash('message')});
+});
+router.get('/success', function(req, res) {
+  console.log('** app.get /success **')
+  res.render('success')
+})
+router.get('/fail', function(req, res) {
+  console.log('** app.get /fail **')
+  res.render('fail')
+})
+router.get('/logout', function(req, res) {
+  req.session.destroy();
+  req.logout();
+  res.redirect('/signin');
 });
 
 module.exports = router;
